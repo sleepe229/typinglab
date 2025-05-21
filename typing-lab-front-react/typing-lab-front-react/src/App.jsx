46552f825409a38610517fca14083661345e26d1
Новиков Никита Сергеевич <nik.n.novikov.2003@gmail.com>
2025-05-22 02:16:23 +0300

    тренировОЧКА version1

--- typing-lab-front-react/typing-lab-front-react/src/App.jsx
+++ typing-lab-front-react/typing-lab-front-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
+import TypingTest from "./pages/typingtest/TypingTest.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Landing />} />
+        <Route path='/tipingtest' element={<TypingTest />} />
       </Routes>
     </Router>
   )
